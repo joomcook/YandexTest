9560e3a75ae88a28949d8dee76d54bc148e6ab6e
Alexey <joomcook@gmail.com>
2020-10-04 12:31:30 +0300

    Added encoding convert

--- src/main/java/pages/YandexHomePage.java
+++ src/main/java/pages/YandexHomePage.java
@@ -1,10 +1,13 @@
 package pages;
 
 import base.TestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
 
@@ -21,8 +24,10 @@ public class YandexHomePage extends TestBase {
         String YandexPageTitle;
 
         YandexPageTitle = driver.getTitle();
+        ByteBuffer YandexPageTitleUTF8 = StandardCharsets.UTF_8.encode(YandexPageTitle);
+        ByteBuffer YandexPageTitleToCompareUTF8 = StandardCharsets.UTF_8.encode("Яндекс");
 
-        if (YandexPageTitle.equals("Яндекс")) {
+        if (YandexPageTitleUTF8.equals(YandexPageTitleToCompareUTF8)) {
             return true;
         }
 
